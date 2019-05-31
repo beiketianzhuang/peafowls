8208233e6c9f315a4da4957e650f5f11b5794b4a
chenlang <chenlang@purang.com>
2019-05-31 18:09:32 +0800

    部署页效果添加

--- srcs/src/components/BuildDeploy.vue
+++ srcs/src/components/BuildDeploy.vue
@@ -11,15 +11,27 @@
                     :data="tableData"
                     style="width: 100%">
                 <el-table-column
-                        prop="context"
-                        label="应用"
+                        prop="version"
+                        label="版本"
                         width="180">
                 </el-table-column>
                 <el-table-column
                         label="构建状态"
                         width="240">
                     <template slot-scope="scope">
-                        <el-progress :percentage="90" status="success"></el-progress>
+                        <div v-if="scope.row.buildStatus == 'building'">
+                            <span @click="dialogTableVisible = true" style="color:#409eff">
+                                <a href="#">
+                                    查看日志
+                                </a>
+                            </span>
+                            <el-progress :percentage="scope.row.percentage" status=""></el-progress>
+                        </div>
+                        <div v-else>
+                            <el-badge is-dot class="item" :type="scope.row.badge">
+                            </el-badge>
+                            <span :style="{color:scope.row.color}">{{scope.row.status}}</span>
+                        </div>
                     </template>
                 </el-table-column>
                 <el-table-column
@@ -45,7 +57,11 @@
             </div>
 
         </el-card>
+        <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+            <iframe  src="http://10.10.31.25:8080/job/sqcsbackend_prod/4/console">
 
+            </iframe>
+        </el-dialog>
     </div>
 
 </template>
@@ -54,26 +70,43 @@
     export default {
         data() {
             return {
+                dialogTableVisible:false,
                 tableData: [{
-                    buildStatus: '2016-05-02',
-                    context: 'devops',
-                    codeChange: '代码变更'
+                    buildStatus: 'building',
+                    version: '4',
+                    codeChange: '代码变更',
+                    status: '构建中',
+                    badge: 'info',
+                    color:'unknown',
+                    percentage:80
                 }, {
-                    buildStatus: '2016-05-04',
-                    context: 'devops',
-                    codeChange: '代码变更'
+                    buildStatus: 'success',
+                    version: '3',
+                    codeChange: '代码变更',
+                    status: '构建成功',
+                    badge: 'success',
+                    color:'green',
+                    percentage:100
                 }, {
-                    buildStatus: '2016-05-01',
-                    context: 'devops',
-                    codeChange: '代码变更'
+                    buildStatus: 'failed',
+                    version: '2',
+                    codeChange: '代码变更',
+                    status: '构建失败',
+                    badge: 'danger',
+                    color:'red',
+                    percentage:100
                 }, {
-                    buildStatus: '2016-05-03',
-                    context: 'devops',
-                    codeChange: '代码变更'
+                    buildStatus: 'unstable',
+                    version: '1',
+                    codeChange: '代码变更',
+                    status: '构建不稳定',
+                    badge: 'warning',
+                    color:'#E6A23C',
+                    percentage:100
                 }]
             }
         },
-        methods:{
+        methods: {
             buildStatusFormat() {
                 return "失败";
             }
@@ -96,6 +129,7 @@
         display: table;
         content: "";
     }
+
     .clearfix:after {
         clear: both
     }
@@ -103,5 +137,9 @@
     .box-card {
         width: 900px;
         margin: auto;
+    }
+
+    .item {
+        margin-top: 20px;
     }
 </style>
