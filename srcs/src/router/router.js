9036c23dc84c24dd26287e644dce7afd4f868d34
chenlang <chenlangkk@163.com>
2019-06-04 15:26:00 +0800

    添加应用列表页

--- srcs/src/router/router.js
+++ srcs/src/router/router.js
@@ -1,7 +1,11 @@
 import BuildDeploy from '../components/BuildDeploy';
+import ContextList from '../components/ContextList';
 
 const routers = [{
     path: '/',
+    component: ContextList
+},{
+    path: '/deploy',
     component: BuildDeploy
 }];
 export default routers;
