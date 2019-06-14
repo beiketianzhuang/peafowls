cf16289dcbd855de927135ccf5890ff9110883ce
chenlang <chenlangkk@163.com>
2019-06-14 17:59:24 +0800

    添加admin监控

--- src/main/java/com/lchen/ccdeploy/CcDeployApplication.java
+++ src/main/java/com/lchen/ccdeploy/CcDeployApplication.java
@@ -1,9 +1,10 @@
 package com.lchen.ccdeploy;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@EnableAdminServer
 @EnableScheduling
 @SpringBootApplication
 public class CcDeployApplication {
