9b1061a5dd0523eff33c2b86594807a80e31d3d9
lchen <lchen@easipay.net>
2019-06-24 22:01:15 +0800

    调整类中方法

--- walle-api-admin/src/main/java/com/lchen/walleapiadmin/helper/ApiHttpHelper.java
+++ walle-api-admin/src/main/java/com/lchen/walleapiadmin/helper/ApiHttpHelper.java
@@ -10,25 +10,22 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-import static java.lang.String.format;
-import static java.nio.charset.Charset.defaultCharset;
-import static java.util.Objects.nonNull;
+import static com.lchen.walleapiadmin.model.resp.ApiResp.apiRespBuilder;
 import static java.util.Optional.ofNullable;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
-import static org.apache.http.HttpStatus.SC_MOVED_TEMPORARILY;
-import static org.apache.http.HttpStatus.SC_OK;
 
 @Slf4j
 @Component
 public class ApiHttpHelper implements ApiHttpConnection {
 
-    private static final String REQUEST_ERROR_MESSAGE = "请求状态异常：%d";
-    private static final String HEADER_LOCATION = "Location";
+    @Value("${api.request.socket.timeout:5000}")
+    private Integer socketTimeout;
+    @Value("${api.request.connect.timeout:100}")
+    private Integer connectTimeout;
 
     @Override
     public void close() {
@@ -39,7 +36,7 @@ public class ApiHttpHelper implements ApiHttpConnection {
         ApiResp apiResp = new ApiResp();
         CloseableHttpClient client = HttpClientBuilder.create().build();
         try {
-            RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(5000).setConnectTimeout(5000).build();
+            RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(socketTimeout).setConnectTimeout(connectTimeout).build();
             requestBase.setConfig(requestConfig);
             HttpResponse response = client.execute(requestBase);
             HttpEntity entity = response.getEntity();
@@ -58,22 +55,6 @@ public class ApiHttpHelper implements ApiHttpConnection {
                 .apply(apiRunnerHistory.getRequestUrl(), apiRunnerHistory.getRequestParams(), apiRunnerHistory.getRequestHeaders());
     }
 
-    private ApiResp apiRespBuilder(HttpEntity entity, HttpResponse response) {
-        String responseContent = EMPTY;
-        try {
-            if (response.getStatusLine().getStatusCode() != SC_OK) {
-                responseContent = format(REQUEST_ERROR_MESSAGE, response.getStatusLine().getStatusCode());
-                if (response.getStatusLine().getStatusCode() == SC_MOVED_TEMPORARILY && nonNull(response.getFirstHeader(HEADER_LOCATION))) {
-                    responseContent = responseContent.concat("；Redirect地址：" + response.getFirstHeader(HEADER_LOCATION).getValue());
-                }
-            }
-            responseContent = responseContent.concat(EntityUtils.toString(entity, defaultCharset()));
-            EntityUtils.consume(entity);
-        } catch (Exception e) {
-            log.error("获取返回信息失败", e);
-        }
-        return ApiResp.builder().message(responseContent).status(response.getStatusLine().getStatusCode()).build();
-    }
 
     private void closeAllConnection(HttpRequestBase requestBase, CloseableHttpClient client) {
         ofNullable(requestBase).ifPresent(HttpRequestBase::releaseConnection);
--- walle-api-admin/src/main/java/com/lchen/walleapiadmin/model/resp/ApiResp.java
+++ walle-api-admin/src/main/java/com/lchen/walleapiadmin/model/resp/ApiResp.java
@@ -2,14 +2,24 @@ package com.lchen.walleapiadmin.model.resp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import static java.lang.String.format;
+import static java.nio.charset.Charset.defaultCharset;
+import static java.util.Objects.nonNull;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.http.HttpStatus.SC_MOVED_TEMPORARILY;
+import static org.apache.http.HttpStatus.SC_OK;
 
 /**
  * @author : lchen
  * @date : 2019/6/24
  */
-@Data
+@Slf4j
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,4 +27,48 @@ public class ApiResp {
     private int status;
     private String message;
     private String errorMessage;
+
+    private static final String REQUEST_ERROR_MESSAGE = "请求状态异常：%d";
+    private static final String HEADER_LOCATION = "Location";
+
+    public static ApiResp apiRespBuilder(HttpEntity entity, HttpResponse response) {
+        String responseContent = EMPTY;
+        try {
+            if (response.getStatusLine().getStatusCode() != SC_OK) {
+                responseContent = format(REQUEST_ERROR_MESSAGE, response.getStatusLine().getStatusCode());
+                if (response.getStatusLine().getStatusCode() == SC_MOVED_TEMPORARILY && nonNull(response.getFirstHeader(HEADER_LOCATION))) {
+                    responseContent = responseContent.concat("；Redirect地址：" + response.getFirstHeader(HEADER_LOCATION).getValue());
+                }
+            }
+            responseContent = responseContent.concat(EntityUtils.toString(entity, defaultCharset()));
+            EntityUtils.consume(entity);
+        } catch (Exception e) {
+            log.error("获取返回信息失败", e);
+        }
+        return ApiResp.builder().message(responseContent).status(response.getStatusLine().getStatusCode()).build();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
