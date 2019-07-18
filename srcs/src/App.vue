2190bfe829946af1b63c1447f9afaa5ec6bcfaba
chenlang <chenlangkk@163.com>
2019-07-18 10:01:15 +0800

    添加api详情页

--- srcs/src/App.vue
+++ srcs/src/App.vue
@@ -1,70 +1,101 @@
 <template>
-  <div id="app">
-    <!--<span>发布平台</span>-->
-    <el-container style="height: 900px">
-      <el-aside style="background-color:#545c64"  width="200px">
-        <el-col>
-          <el-menu
-                  default-active="2"
-                  class="el-menu-vertical-demo"
-                  background-color="#545c64"
-                  text-color="#fff"
-                  active-text-color="#ffd04b">
-            <el-menu-item index="1">
-              <!--<i class="el-icon-menu"></i>-->
-              <span slot="title" style="font-size: 30px">发布平台</span>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <router-link to="/" slot="title"><span >应用列表</span></router-link>
-            </el-menu-item>
-            <el-menu-item index="3">
-              <i class="el-icon-menu"></i>
-              <router-link to="/monitor" slot="title"><span >应用监控中心</span></router-link>
-            </el-menu-item>
-            <el-menu-item index="3">
-              <i class="el-icon-menu"></i>
-              <router-link to="/apiManager" slot="title"><span >API管理中心</span></router-link>
-            </el-menu-item>
-          </el-menu>
-        </el-col>
-      </el-aside>
-      <el-container>
-        <el-header style="background-color: white">
-        </el-header>
-        <el-main> <div style="height:100%">
-          <router-view></router-view>
-        </div></el-main>
-        <!--<el-footer>Footer</el-footer>-->
-      </el-container>
-    </el-container>
+    <div id="app">
+        <!--<span>发布平台</span>-->
+        <el-container style="height: 900px">
+            <el-aside style="background-color:#545c64" width="200px">
+                <el-col>
+                    <el-menu
+                            default-active="2"
+                            class="el-menu-vertical-demo"
+                            background-color="#545c64"
+                            text-color="#fff"
+                            active-text-color="#ffd04b">
+                        <el-menu-item index="1">
+                            <!--<i class="el-icon-menu"></i>-->
+                            <span slot="title" style="font-size: 30px">walle</span>
+                        </el-menu-item>
+                        <el-menu-item index="2">
+                            <i class="el-icon-menu"></i>
+                            <router-link to="/" slot="title"><span style="color: azure">应用列表</span></router-link>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <i class="el-icon-menu"></i>
+                            <router-link to="/monitor" slot="title"><span style="color: azure">应用监控中心</span>
+                            </router-link>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <i class="el-icon-menu"></i>
+                            <router-link to="/apiManager" slot="title"><span style="color: azure">API管理中心</span>
+                            </router-link>
+                        </el-menu-item>
+                        <el-menu-item index="4">
+                            <i class="el-icon-menu"></i>
+                            <router-link to="/apiManager" slot="title"><span style="color: azure">告警中心</span>
+                            </router-link>
+                        </el-menu-item>
+                        <el-menu-item index="5">
+                            <i class="el-icon-menu"></i>
+                            <router-link to="/apiManager" slot="title"><span style="color: azure">日志管理</span>
+                            </router-link>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <i class="el-icon-menu"></i>
+                            <router-link to="/apiManager" slot="title"><span style="color: azure">配置中心</span>
+                            </router-link>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <i class="el-icon-menu"></i>
+                            <router-link to="/apiManager" slot="title"><span style="color: azure">调度中心</span>
+                            </router-link>
+                        </el-menu-item>
+                    </el-menu>
+                </el-col>
+            </el-aside>
+            <el-container>
+                <el-header style="background-color: white">
+                </el-header>
+                <el-main>
+                    <div style="height:100%">
+                        <router-view></router-view>
+                    </div>
+                </el-main>
+                <!--<el-footer>Footer</el-footer>-->
+            </el-container>
+        </el-container>
 
-  </div>
+    </div>
 </template>
 
 <script>
 
-export default {
-  data() {
-    return {
-      activeIndex: '1',
-      activeIndex2: '1'
-    };
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
+    export default {
+        data() {
+            return {
+                activeIndex: '1',
+                activeIndex2: '1'
+            };
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
     }
-  }
-}
 </script>
 
 <style>
 
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    /*text-align: center;*/
-  }
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        /*text-align: center;*/
+    }
 
+    .router-link-active {
+        text-decoration: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
 </style>
--- srcs/src/components/ApiDetails.vue
+++ srcs/src/components/ApiDetails.vue
@@ -1,34 +1,64 @@
 <template>
     <div>
-        <el-card class="box-card">
-            <div slot="header" class="clearfix">
-                <span>接口信息</span>
-                <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-            </div>
-        </el-card>
-        <el-card class="box-card" style="margin-top: 15px">
-            <div slot="header" class="clearfix">
-                <span>请求头</span>
-                <el-link :underline="false" style="float: right;font-size: 25px">
-                    <i class="el-icon-circle-plus-outline"></i>
-                </el-link>
-            </div>
-        </el-card>
-        <el-card class="box-card" style="margin-top: 15px">
-            <div slot="header" class="clearfix">
-                <span>请求参数</span>
-                <el-link :underline="false" style="float: right;font-size: 25px">
-                    <i class="el-icon-circle-plus-outline"></i>
-                </el-link>
-            </div>
-        </el-card>
+        <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" label-width="100px" class="demo-dynamic">
+            <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>接口信息</span>
+                    <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                </div>
+            </el-card>
+            <el-card class="box-card" style="margin-top: 15px">
+                <div slot="header" class="clearfix">
+                    <span>请求头</span>
+                    <el-link :underline="false" style="float: right;font-size: 25px">
+                        <i  @click="addHead" class="el-icon-circle-plus-outline"></i>
+                    </el-link>
+                </div>
+                <el-form-item
+                        v-for="(domain, index) in dynamicValidateForm.domains"
+                        :key="domain.key"
+                        :rules="{
+      required: true, message: '域名不能为空', trigger: 'blur'
+    }"
+                >
+                    <el-input v-model="domain.value"></el-input>
+                    <el-button @click.prevent="removeDomain(domain)">删除</el-button>
+                </el-form-item>
+            </el-card>
+            <el-card class="box-card" style="margin-top: 15px">
+                <div slot="header" class="clearfix">
+                    <span>请求参数</span>
+                    <el-link :underline="false" style="float: right;font-size: 25px">
+                        <i class="el-icon-circle-plus-outline"></i>
+                    </el-link>
+                </div>
+            </el-card>
+        </el-form>
     </div>
 
 </template>
 
 <script>
     export default {
-        name: "ApiDetails"
+        name: "ApiDetails",
+        data() {
+            return {
+                dynamicValidateForm: {
+                    domains: [{
+                        value: ''
+                    }],
+                    email: ''
+                }
+            };
+        },
+        methods:{
+            addHead() {
+                this.dynamicValidateForm.domains.push({
+                    value: '',
+                    key: Date.now()
+                });
+            }
+        }
     }
 </script>
 
