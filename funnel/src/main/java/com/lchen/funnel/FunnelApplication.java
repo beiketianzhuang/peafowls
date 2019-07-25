66049df1c41a348a3cec1d5234e9e09dd99bce1d
lchen <lchen@easipay.net>
2019-07-25 21:27:06 +0800

    添加网关路由应用

---
+++ funnel/src/main/java/com/lchen/funnel/FunnelApplication.java
@@ -0,0 +1,13 @@
+package com.lchen.funnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunnelApplication.class, args);
+    }
+
+}
